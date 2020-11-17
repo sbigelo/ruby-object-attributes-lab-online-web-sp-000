c1a5965d90d44803b2ac35d6ef1b9c56861c87d9
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-17 22:58:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,4 @@
 class Person
-  def name=(person)
-    @name = person
-  end
-  
-  def name
-    @name
-  end
-  
-  def job=(person)
-    @job = person
-  end
-  
-  def job
-    @job
-  end
+attr_accessor :name, :job
   
 end
