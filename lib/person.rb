4ca1fd9770a8e58cf680acd501a0e2efc639e7db
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-17 22:56:36 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(person)
+    @name = person
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(person)
+    @job = person
+  end
+  
+  def job
+    @job
+  end
+  
+end
